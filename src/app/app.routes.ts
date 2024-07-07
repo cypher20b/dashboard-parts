fbdeae4e154796d59301079ae0c814facec17ec6
cypher20b <nuel.emma20@gmail.com>
2024-07-07 12:32:03 +0000

    feat: added two more routes to the route.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { ChartsComponent } from './components/charts/charts.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { LoadingSpinnersComponent } from './components/loading-spinners/loading-spinners.component';
 
 export const routes: Routes = [
-    {path:'dashboard', component:ChartsComponent}
+    {path:'', component:LoadingSpinnersComponent},
+    {path:'dashboard', component:CarouselComponent}
 ];
